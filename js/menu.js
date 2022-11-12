c6a69e69ad2c562939f8021bc17a74d39b025fa5
ode-oghenerume <114571239+ode-oghenerume@users.noreply.github.com>
2022-11-12 10:14:38 +0100

    update
    nav bar added, slide show added.

---
+++ js/menu.js
@@ -0,0 +1,41 @@
+// var boy = {
+//     "name":"Jerry",
+//     "age": 45
+// }
+
+// var girl= {
+//     "name":"Princess",
+//     "age": 67
+// }
+
+// if(boy.age>girl.age){
+//     console.log(boy.name + "is older than" + girl.name)
+// }
+//     else{
+//         console.log(boy.name + "is not older than" + girl.name)
+//     }
+
+
+// Functions are lines of code you do not want to execute until they are called upon
+
+// function change(){
+//     var my = document.getElementById("d")
+
+//     my.innerHTML="Welcome to Javascript"
+// }
+
+var menu = document.getElementById("menu_items")
+var is_menu_open = false
+
+function open_items(){
+     if(is_menu_open)
+     {
+        is_menu_open = false
+        menu.classList.add("options")
+        
+   }else{
+        is_menu_open = true
+        menu.classList.remove("options")
+    }
+}
+
